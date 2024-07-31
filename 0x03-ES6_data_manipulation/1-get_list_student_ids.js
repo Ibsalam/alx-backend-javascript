17984d8a94c626bb1802a37beab870f05193336b
ibsalam <asio4edu@gmail.com>
2024-07-31 07:42:18 +0000

    map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -9,8 +9,8 @@
 
 export default function getListStudentIds(list) {
   if (!Array.isArray(list)) {
-    return [];
+    return list.map((student) => student.id);
   }
 
-  return list.map((student) => student.id);
+  return [];
 }
